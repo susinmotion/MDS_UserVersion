d3025cf404607a49db530d6c48b6a905cb605955
SusInMotion <steinman.tutoring@gmail.com>
2016-02-23 14:47:28 -0500

    making a clean, gui-enabled version of MDS software

---
+++ shell.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+make
+python run.py
+
+head -n 2 config.cfg>tmp.cfg
+source tmp.cfg
+arr=$(echo $FILENAMES | tr "," "\n")
+echo ${arr[*]}
+
+
+if [ "$ZIPPED" = "True" ]
+	then
+		gunzip -c ${arr[*]} | ./secondtrie
+
+else
+	for f in ${arr[*]}
+    do
+        echo "Processing $f"
+        sed -i '' -e '$a\' $f
+    done
+    cat ${arr[*]} | ./secondtrie
+fi
+rm tmp.cfg
